32f447db2c2b40397b829690c76902acaaec0ed1
Ejimenez1993 <82065964+Ejimenez1993@users.noreply.github.com>
2021-04-13 12:06:08 -0600

    Diseño del menú.
    Primer versión del diseño del formulario menú.

---
+++ Control de Inventario/FrmMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_de_Inventario
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBodega_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntTransacciones_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntInventario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnArticulos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntProveedores_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntReportes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
